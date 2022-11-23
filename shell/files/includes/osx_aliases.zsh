2da97190fc235cdb37ece9e43f3112cfd0ff353d
Manuel Coppotelli <manuelcop@gmail.com>
2022-11-23 14:56:52 +0100

    Update osx_aliases.zsh

--- shell/files/includes/osx_aliases.zsh
+++ shell/files/includes/osx_aliases.zsh
@@ -1,5 +1,5 @@
 # IP addresses
-alias ip="curl https://diagnostic.opendns.com/myip ; echo"
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
 # Flush Directory Service cache
