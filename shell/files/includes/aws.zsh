d4886061a52e30f3bf0167e712d02f2cc760cfcf
Manuel Coppotelli <manuelcoppotelli@users.noreply.github.com>
2023-10-28 11:39:44 +0200

    Update aws.zsh

--- shell/files/includes/aws.zsh
+++ shell/files/includes/aws.zsh
@@ -1,15 +1,7 @@
 export AWS_PAGER=''
 # export AWS_CLI_AUTO_PROMPT=on
-export AWS_CONFIGURE_SSO_DEFAULT_SSO_START_URL=https://d-9999999999.awsapps.com/start
-export AWS_CONFIGURE_SSO_DEFAULT_SSO_REGION=eu-west-1
 
-sso() {
-  unset AWS_PROFILE
-  export AWS_PROFILE=$1
-
-  aws sts get-caller-identity &> /dev/null || \
-    aws sso login || \
-    (unset AWS_PROFILE && aws-sso-util credential-process --profile $1)
-
-  eval $(aws-export-credentials --env-export)
-}
+if command -v aws_completer &> /dev/null; then
+  autoload -Uz bashcompinit && bashcompinit
+  complete -C aws_completer aws
+fi
